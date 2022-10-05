aff1976717e57be3b8f3f9e94263e5371b89116b
Kaitokids872001 <nguyen872001@gmail.com>
2022-10-05 07:46:37 +0700

    Add: BaseController and CreateController

---
+++ WebApplication1/Controllers/tokenController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Nodes;
+
+namespace UngDungBanHang.Api.Controllers
+{
+    [ApiController]
+    [Route("/api/cmd/[controller]")]
+    public class tokenController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public JsonNode Index([FromBody] JsonObject index)
+        {
+            Token_Input input = JsonConvert.DeserializeObject<Token_Input>(index.ToString());
+            Token_Output output = new Token_Output();
+            output.Query_DataInput(input);
+            return JsonObject.Parse(JsonConvert.SerializeObject(output));
+        }
+    }
+}
